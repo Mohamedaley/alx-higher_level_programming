1301d3f0f7fe1f70b02d803a8476468465271c83
vulturepremier <vulturepremier@gmail.com>
2024-03-12 18:13:01 -0700

    New commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if (num < 10):
-        print("0{}".format(num), end=", ")
-    elif num != 99:
-        print("{}".format(num), end=", ")
-    else:
-        print("{}".format(num))
+for index in range(0, 99):
+    print("{:02d}, ".format(index), end="")
+print("99")
