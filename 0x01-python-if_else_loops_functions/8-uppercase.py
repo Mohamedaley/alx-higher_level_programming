073c965fb48a53f818b87a912cde65cba5a2af18
vulturepremier <vulturepremier@gmail.com>
2024-03-13 15:21:33 +0200

    New commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    while (str[i]):
-        if ord(str[i]) >= 65 and ord(str[i]) <= 90:
-            new = str[i]
-        else:
-            new = ord(str[i]) + 32
-        print("{}".format(new), end="")
+    for index in str:
+        if ord(index) >= 97 and ord(index) <= 122:
+            index = chr(ord(index) - 32)
+        print("{}".format(index), end="")
+    print("")
