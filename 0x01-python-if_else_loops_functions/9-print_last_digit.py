d4a53a5be508aeb1bee7980b01b89b4f33641afb
vulturepremier <vulturepremier@gmail.com>
2024-03-13 15:28:42 +0200

    New commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         number = number * -1
-        new = (number % 10) * -1
     else:
         new = number % 10
     print("{}".format(new), end="")
+    return (new)
