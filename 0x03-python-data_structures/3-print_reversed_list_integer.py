b21e7552ac83d3fec474fe4c27802c7323f6824f
vulturepremier <vulturepremier@gmail.com>
2024-03-16 18:30:52 +0200

    New commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         my_list.reverse()
-        for item in new:
+        for item in my_list:
             print("{:d}".format(item))
