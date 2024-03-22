3f546a132d2e18ead892937acac405bc3d6e2860
vulturepremier <vulturepremier@gmail.com>
2024-03-22 20:57:08 +0200

    New commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
     new = []
     for item in my_list:
         if item == search:
             new.append(replace)
         else:
-            new.append(item)
+            new.append(element)
+    return new
