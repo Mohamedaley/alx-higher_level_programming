fee26c0eae0ba62811caab2914a0a181d9ec9aad
vulturepremier <vulturepremier@gmail.com>
2024-03-23 00:39:30 +0200

    New commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def multiply_by_2(a_dictionary):
     new = {}
-    for item in a_dictionary.items():
+    for key, value in a_dictionary.items():
         new.update({key: (value * 2)})
-return new
+    return new
