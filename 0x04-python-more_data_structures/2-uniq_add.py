8ef343eec5278c4382fb193dee837558e72566ea
vulturepremier <vulturepremier@gmail.com>
2024-03-22 21:04:31 +0200

    New commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
     new = []
     sum = 0
     for item in my_list:
         if item not in new:
-            new.append(item)
             sum += item
-return sum
+            new.append(item)
+    return sum
