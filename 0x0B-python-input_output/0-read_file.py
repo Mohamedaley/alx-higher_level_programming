d1d159dd6cc4dd4022e5f31bd298b30300df3042
vulturepremier <vulturepremier@gmail.com>
2024-04-16 19:34:15 +0200

    New commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
 def read_file(filename=""):
-    with (open(filename, encoding="utf-8")) as file:
-        data = file.read()
-    print(data)
+    """Read and print the contents of a text file in using utf-8."""
+    with open(filename, encoding="utf-8") as file:
+        print(file``.read(), end="")
