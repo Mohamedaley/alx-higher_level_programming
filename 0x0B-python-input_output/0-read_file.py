86bdfd4f5eef43ebda514fe0dc60f3ec0629de05
vulturepremier <vulturepremier@gmail.com>
2024-04-16 19:35:41 +0200

    New mod

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a text file-reading function."""
 
+
 def read_file(filename=""):
     """Read and print the contents of a text file in using utf-8."""
     with open(filename, encoding="utf-8") as file:
